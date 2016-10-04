4bb9b5fce5e7e3d74c709f8d494e9e0204f9147f
jin camou and sailor winkelman <sailor@>
2016-10-03 19:39:32 -0700

    Add html and js files. Add function to return false for a year that is not a leap year.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//business logic
+var leapYear = function(year) {
+    return false;
+};
+
+
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
