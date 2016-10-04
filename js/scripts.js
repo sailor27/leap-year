9091a8bb34b51084fa5550f0eafda4d1f6ae1f61
jin camou and sailor winkelman <jin@>
2016-10-03 19:54:10 -0700

    Add function to return true for a year that is evenly divisible by 4. Add function to return false for a year that is divisible by 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,39 @@
 //business logic
+
+//return false for a year that is not a leap year
 var leapYear = function(year) {
     return false;
 };
 
+//return true for a year that is divisible by 4
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
+//return false for a year that is divisible by 100
 
+var leapYear = function(year) {
+  if (year % 100 === 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
+/*//return false for a year that is divisible by 100
+var leapYear = function(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}; */
+
+//front-end logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
